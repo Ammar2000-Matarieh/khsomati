6cb3768b2c6018e9b000a8beee2d652cafac9dbb
ahmadalali98 <155850460+ahmadalali98@users.noreply.github.com>
2025-11-13 19:33:52 +0300

    a

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -71,15 +71,34 @@ class _LoginScreenState extends State<LoginScreen> {
                                   borderRadius: BorderRadius.circular(16),
                                 ),
                               ),
-                            );
-                            print(state.message);
-                          } else if (state is CodeSentState) {
-                          //  Navigator.pushReplacementNamed(
-                          //     context,
-                          //     RouteString.otpScreen,
-                          //   );
-                          }
-                        },
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: SizedBox(
+                              height: 50,
+                              child: CustemTextFormFelid(
+                                controller: phoneNumber,
+                                keyboardType: TextInputType.number,
+                                hintText: 'Enter your phone number',
+                                labelText: 'Enter your phone number',
+                                prefixIcon: Icon(
+                                  Icons.phone,
+                                  color: Colors.grey,
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please Enter Your Phone Number';
+                                  } else if (value.length < 9) {
+                                    return 'Phone number too short';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
 
                       SizedBox(height: 30),
