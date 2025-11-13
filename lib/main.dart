f6e30c2d2fff0f243bf62e6a08569b87ed7e21aa
AmmarMatarieh2000 <ammarmatarieh2@gmail.com>
2025-11-13 15:03:09 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,20 @@ import 'package:khsomati/firebase_options.dart';
 import 'package:khsomati/router/route.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  runApp( Khosomati());
+  runApp(Khosomati());
 }
 
-class Khosomati extends StatelessWidget{
+class Khosomati extends StatelessWidget {
+  const Khosomati({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      title: "Khosomati App",
+      debugShowCheckedModeBanner: false,
       routes: routes,
     );
   }
-
 }
-
--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+    // Future.delayed(const Duration(seconds: 5), () {
+    //   Navigator.pushReplacementNamed(context, '/home');
+    // });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: Image.asset("assets/logos/logo_app.PNG", width: 200)),
+    );
   }
 }
