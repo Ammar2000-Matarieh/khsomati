9fa98194937f1c7f246ad87dd473654c7093e6fb
MKmasri2002 <almasrimahmood98@gmail.com>
2025-11-13 15:21:34 +0300

    w

--- lib/router/route_string.dart
+++ lib/router/route_string.dart
@@ -1,5 +1,6 @@
 class RouteString {
   static const String splash = '/';
   static const String onborading = '/onborading';
+  static const String login = '/login';
   static const String home = '/home';
 }
